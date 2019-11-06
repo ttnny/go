e4c17872b30d78be25d2065ed779ffa51fc1c2e8
ttnny <ttnny@users.noreply.github.com>
2019-11-06 02:30:59 -0800

    Update interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -6,6 +6,8 @@ type bot interface {
 	getGreeting() string
 }
 
+type empty interface{} // or just: var any interface{}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -15,6 +17,16 @@ func main() {
 
 	printGreeting(eb)
 	printGreeting(sb)
+
+	// Try empty interface
+	var any empty
+	any = []int {1, 2, 3}
+	any = map[int]bool{1: true, 2: false}
+	any = "Hello"
+	any = 3
+
+	// any = any * 2 <-- this won't compile, need type assertion to extract number
+	any = any.(int) * 2
 }
 
 func printGreeting(b bot) {
