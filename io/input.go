7d09c1cbf708d203a1fdd195c48b323b875f169e
ttnny <ttnny@users.noreply.github.com>
2020-04-05 03:50:54 -0700

    Update

--- io/input.go
+++ io/input.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -46,4 +47,16 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(rel)
+
+	// Environment variables
+
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		fmt.Println(pair[0])
+	}
 }
